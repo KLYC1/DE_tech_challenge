6367db7aca4fc203689d550b222737c3eaa62154
KLYC1 <126397226+KLYC1@users.noreply.github.com>
2023-03-17 01:19:15 +0800

    update main function 1
    update main function with the following:
    - drop NaN values in 'name' dataframe column 
    - split name into "first_name" & "last_name", add new columns 
    - remove invalid date "1996/02/31" 
    - format date_of_birth into YYYYMMDD
    - validity check for mobile number with 8 digits, age above 18 & valid emails ending with ".com" and ".net"
    - create new "membership_id" column with SHA-256 hash to 'last_name','date_of_birth' column truncated to first 5 digits of hash
    - drop unwanted columns, rearrange columns & output to csv file "successful.csv" for successful applications

--- section1/airflow_sec1.py
+++ section1/airflow_sec1.py
@@ -49,6 +49,12 @@ def check_files():
                     pass
     return my_list
 
+# Hash a tuple of values using SHA-256 and return only the first 5 characters
+def sha256_hash(values):
+    str_values = ''.join(str(v) for v in values)
+    full_hash = hashlib.sha256(str_values.encode('utf-8')).hexdigest()
+    return full_hash[:5]
+
 # Main function to concat raw dataframes, clean and verify the data
 def main():
     raw_df = pd.DataFrame()
@@ -57,6 +63,32 @@ def main():
     for filenames in my_list:
         new_df = pd.read_csv(input_dir + str(filenames), header=0)
         raw_df = pd.concat([raw_df, new_df], axis=0, ignore_index=True)
+        
+    # drop row if name is NaN and split 'name' column into 2 columns and add into existing dataframe
+    valid_df = raw_df.dropna(axis=0, how='any', subset=['name'], inplace=False)
+    valid_df[['first_name','last_name']] = valid_df.name.str.split(" ", 1, expand=True)
+
+    # remove row contains invalid date "31 feb 1996" & format date_of_birth column into YYYYMMDD format
+    i = valid_df[(valid_df.date_of_birth == '1996/02/31')].index
+    print(i)
+    valid_df = valid_df.drop(i)
+    valid_df['date_of_birth'] = pd.to_datetime(valid_df.date_of_birth).dt.strftime('%Y%m%d')
+    
+    # Check validity of mobile_no, age above 18 and email, remove rows which are not valid (value 0)
+    valid_df['valid_mobile_no'] = valid_df['mobile_no'].apply(lambda x: 1 if len(str(x)) == 8 else 0)
+    valid_df['above_18'] = valid_df['date_of_birth'].apply(lambda x: 1 if x < '20040101' else 0)
+    valid_df['valid_email'] = valid_df['email'].apply(lambda x: 1 if x.endswith('.com') or x.endswith('.net') else 0)
+    index_names = valid_df[(valid_df['valid_mobile_no'] == 0) | (valid_df['above_18'] == 0) | (valid_df['valid_email'] == 0 )].index
+    valid_df = valid_df.drop(index_names)
+    
+    # Apply truncated SHA-256 hash to the 'last_name','date_of_birth' column
+    valid_df['membership_id'] = valid_df[['last_name','date_of_birth']].apply(tuple, axis=1).apply(sha256_hash)
+    
+    # drop unwanted columns, rearrange columns and output valid applications to successful dir
+    success_df = valid_df.drop(columns=['name', 'valid_mobile_no', 'above_18', 'valid_email'], axis=1)
+    success_df = success_df.loc[:,['membership_id','first_name','last_name','email', 'date_of_birth', 'mobile_no']]
+    success_df.to_csv(success_dir+"successful.csv", index=False, encoding='utf-8')
+    print("output success")
 
 main_func = PythonOperator(
     task_id='py_func1',
